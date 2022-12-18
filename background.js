f58261774c392e94e4223b1f073bf0f36a9cce05
Shuhao Zhang <shuhaozhangchris@gmail.com>
2022-12-18 17:22:58 -0500

    rewrite boilerplat by myself

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
+// chrome.tabs is chrome extension's Tab API, which allows us to retrieve info of each tab opened
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
-      });
-    }
-  });
-  
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    // example: https://www.youtube.com/watch?v=0n809nd4Zu4&t=363s
+    // queryParameters = v=0n809nd4Zu4&t=363s
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    // we are at the background.js, this background worker cannot access to the DOM,
+    // it needs to communicate with contentScript.js via some messaging system
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      // ex: 0n809nd4Zu4&t=363s
+      videoId: urlParameters.get("v"),
+      // we can pass anything here, as long as we need
+      // ex: random: "random"
+    });
+  }
+})
